7fd5f3da43c979f61775d4c615e5f865f0860f7a
Bobby Watson <bobby@apstra.com>
2017-07-24 09:31:00 -0400

    Added get, post, put, and delete API methods for os_selector.cfg editing.

--- aeon_ztp/api/views.py
+++ aeon_ztp/api/views.py
@@ -6,13 +6,14 @@
 
 
 import os
+import yaml
 from datetime import datetime
 from os import path
 
 import aeon_ztp
 import pkg_resources
 
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort
 from flask import send_from_directory
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -61,6 +62,78 @@ def api_finalizer(os_name):
     ztp_celery.ztp_finalizer.delay(os_name=os_name, target=from_ipaddr)
     return "OK"
 
+
+@api.route('/api/os_selector/<os_name>', methods=['GET'])
+def api_os_selector_get(os_name):
+    from_dir = path.join(_AEON_TOPDIR, 'etc', 'profiles', os_name, 'os-selector.cfg')
+    os_selector = yaml.safe_load(open(from_dir))
+    return jsonify(os_selector)
+
+
+@api.route('/api/os_selector/<os_name>', methods=['POST'])
+def api_os_selector_post(os_name):
+    """
+    Used for creating/overwriting an entire os-selector.cfg file.
+    :param os_name: Name of the device OS.
+    :request: JSON formatted os-selector.cfg file.
+                Ex: '{"default": {"finally": "finally", "exact_match": "4.16.6M", "image": "EOS-4.16.6M.swi"}'
+    :return:    Updates applied
+                204 if item was updated
+                201 if item was created
+    """
+    from_dir = path.join(_AEON_TOPDIR, 'etc', 'profiles', os_name, 'os-selector.cfg')
+    os_selector = yaml.safe_load(open(from_dir))
+    rqst_data = request.get_json()
+    print rqst_data
+    # Check to see if group already exists in os-selector.cfg
+    if os_selector:
+        status = 204
+    else:
+        status = 201
+    with open(from_dir, 'w') as outfile:
+        yaml.safe_dump(rqst_data, outfile, default_flow_style=False)
+    new_os_selector = yaml.safe_load(open(from_dir))
+    return jsonify(new_os_selector), status
+
+
+@api.route('/api/os_selector/<os_name>', methods=['PUT'])
+def api_os_selector_put(os_name):
+    """
+    Used for appending/updating a single entity (or iterable of entities) into the os-selector.conf file.
+    :param os_name: Name of the device OS.
+    :request: JSON formatted os-selector entry.
+                Ex: '{"default": {"finally": "finally", "exact_match": "4.16.6M", "image": "EOS-4.16.6M.swi"}'
+    :return:    Updates applied
+                204 if item was updated
+                201 if item was created
+    """
+    from_dir = path.join(_AEON_TOPDIR, 'etc', 'profiles', os_name, 'os-selector.cfg')
+    os_selector = yaml.safe_load(open(from_dir))
+    rqst_data = request.get_json()
+    print rqst_data
+    # Check to see if group already exists in os-selector.cfg
+    if all(item in os_selector.items() for item in rqst_data.items()):
+        status = 204
+    else:
+        status = 201
+    os_selector.update(rqst_data)
+    with open(from_dir, 'w') as outfile:
+        yaml.safe_dump(os_selector, outfile, default_flow_style=False)
+    updates = {k: v for (k, v) in os_selector.items() if k in rqst_data.keys()}
+    return jsonify(updates), status
+
+
+@api.route('/api/os_selector/<os_name>/<group_name>', methods=['DELETE'])
+def api_os_selector_delete(os_name, group_name):
+    from_dir = path.join(_AEON_TOPDIR, 'etc', 'profiles', os_name, 'os-selector.cfg')
+    os_selector = yaml.safe_load(open(from_dir))
+    if group_name in os_selector:
+        os_selector.pop(group_name)
+        with open(from_dir, 'w') as outfile:
+            yaml.safe_dump(os_selector, outfile, default_flow_style=False)
+        return group_name
+    else:
+        abort(404)
 
 @api.route('/api/env')
 def api_env():
