6c604a22c7189611de2b0b707bd70fa618acb0d2
Bobby Watson <bobby@apstra.com>
2018-02-27 11:45:32 -0500

    Add support for OPX devices.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class Tox(TestCommand):
 
 setup(
     name="aeon-ztp",
-    version="1.5.2",
+    version="1.5.3",
     author="Apstra Customer Enablement",
     author_email="community@apstra.com",
     description=("AEON ZTP Server"),
